2e87470237069cc3fd4e2555cb0ef3fe77b9bb4a
Qin, Ken <ken.qin@cerner.com>
2018-08-03 16:56:08 -0500

    08/03/2018 - Answers check in.

--- Java/Microsoft/Onsite/LC460-LFUCache.java
+++ Java/Microsoft/Onsite/LC460-LFUCache.java
@@ -1,29 +1,28 @@
 import java.util.HashMap;
-
-import com.sun.jmx.remote.internal.ArrayQueue;
-
 /**
- * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+ * Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
 
 get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
 
 Follow up:
 Could you do both operations in O(1) time complexity?
 
 Example:
 
-LRUCache cache = new LRUCache( 2 /* capacity  );
+LFUCache cache = new LFUCache( 2 - capacity );
 
 cache.put(1, 1);
 cache.put(2, 2);
 cache.get(1);       // returns 1
 cache.put(3, 3);    // evicts key 2
 cache.get(2);       // returns -1 (not found)
-cache.put(4, 4);    // evicts key 1
+cache.get(3);       // returns 3.
+cache.put(4, 4);    // evicts key 1.
 cache.get(1);       // returns -1 (not found)
 cache.get(3);       // returns 3
 cache.get(4);       // returns 4
+ * 
  */
 
 class LFUCache {
@@ -82,9 +81,6 @@ class LFUCache {
     }
     
     public void put(int key, int value) {
-        if (this.capacity == 0) {
-            return;
-        }
         if (get(key) != -1) {
             this.storage.get(key).value = value;
             return;
